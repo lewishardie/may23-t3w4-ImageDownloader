fa4358a55236e9a53ca99a684b2e37ab7681041a
Lewbagz <lewishardie@gmail.com>
2023-11-11 10:10:30 +1100

    index.js skeleton

---
+++ index.js
@@ -0,0 +1,20 @@
+const { downloadPokemonPicture } = require("./downloader.js");
+
+
+
+//-- it will return a Promise
+//-- .then("returned name")
+//-- synch?? way
+downloadPokemonPicture().then(saveFileOutput => {
+    console.log("new image is saved to: " + saveFileOutput);
+}).catch(error => {
+    console.log(error);
+})
+
+//-- async way
+async function exampleDownload(){
+    let savedFileOutput = await downloadPokemonPicture();
+    console.log("new image is saved to: " + saveFileOutput);
+}
+
+exampleDownload();
